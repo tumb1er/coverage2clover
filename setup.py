75a9980eeaa4638efc1c0948ca8410c1fe123b4d
Sergey Tikhonov <zimbler@gmail.com>
2015-09-27 14:19:22 +0300

    classifiers

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
                  'Topic :: Text Processing :: Markup :: XML',
                  'Topic :: Software Development :: Quality Assurance',
                  'Topic :: Software Development :: Testing']
