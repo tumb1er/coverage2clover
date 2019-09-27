c817e01f8d10ec0a51d52163ade4f61955d935a4
Sergey Tikhonov <zimbler@gmail.com>
2019-09-27 17:48:47 +0300

    Update version to 2.0.0 - drop python-2.7 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coverage2clover',
-    version='1.4.0',
+    version='2.0.0',
     packages=['clover'],
     url='http://github.com/tumb1er/coverage2clover',
     license='Beer License',
