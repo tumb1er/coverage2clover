ed55dc7501b942923a6579f66cf76680b8ba335f
tumb1er <zimbler@gmail.com>
2020-09-16 13:22:31 +0300

    Update package metadata for 3.0.0 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="coverage2clover",
-    version="2.0.1",
+    version="3.0.0",
     packages=["clover"],
     url="http://github.com/tumb1er/coverage2clover",
     license="Beer License",
@@ -11,11 +11,11 @@ setup(
     author_email="zimbler@gmail.com",
     description="A tool to convert python-coverage xml report to Atlassian Clover xml report format",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.4",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Text Processing :: Markup :: XML",
         "Topic :: Software Development :: Quality Assurance",
         "Topic :: Software Development :: Testing",
